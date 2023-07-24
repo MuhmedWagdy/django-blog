ebf93bc6461bdf07ee04ffe076cdf6af8a82f88d
muhmedwagdy <mohamed.wagdy2016@gmail.com>
2023-07-24 16:27:03 +0300

    add summernote in form add image

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django_summernote.admin import SummernoteModelAdmin
 
 
+class PostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
 
 
 from .models import Post,Comment
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
 admin.site.register(Comment)
