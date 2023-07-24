fc54a0231cc22e651d0d2a8ee426f2a841108dc1
muhmedwagdy <mohamed.wagdy2016@gmail.com>
2023-07-24 17:00:56 +0300

    add comment class

--- blog/admin.py
+++ blog/admin.py
@@ -6,8 +6,16 @@ from django_summernote.admin import SummernoteModelAdmin
 
 class PostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
     summernote_fields = '__all__'
+    list_display = ['title','draft','author']
+    list_filter = ['author','draft']
+    search_fields = ['content','title']
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user','post','create_at']
+    list_filter = ['user']
+
 
 
 from .models import Post,Comment
 admin.site.register(Post,PostAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment,CommentAdmin)
--- blog/forms.py
+++ blog/forms.py
@@ -11,5 +11,4 @@ class PostForm(forms.ModelForm):
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-
         fields = ['comment']
