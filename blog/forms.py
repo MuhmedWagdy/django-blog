2a12121976cb61ea5f9ffaccede878d82edfdcc3
muhmedwagdy <mohamed.wagdy2016@gmail.com>
2023-07-30 16:16:54 +0300

    add new front end bootstrapp and css front end

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,17 @@
 from django import forms
 from .models import Post,Comment
 
+from django_summernote.admin import SummernoteModelAdmin,SummernoteWidget,SummernoteInplaceWidget
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = ('author',)
+        widgets = {
+            'foo': SummernoteWidget(),
+            'bar': SummernoteInplaceWidget(),
+        }
 
 
 class CommentForm(forms.ModelForm):
